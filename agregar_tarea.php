9f3241b07a7bb02eca9414d9736bf25fcae87330
Paul Vergel <pvergel@tecsup.edu.pe>
2024-09-18 21:26:59 -0500

    Se agregan archivos para manejo de tareas

--- agregar_tarea.php
+++ agregar_tarea.php
@@ -1,3 +1,21 @@
 <?php
 session_start();
+
+$tareas = [];
+
+if (!isset($_SESSION['tareas']))
+{
+    $_SESSION['tareas'] = [];
+}
+
+$tareas = $_SESSION['tareas'];
+
+
+$nueva_tarea = $_POST['tarea'];
+
+$tareas[] = $nueva_tarea;
+
+$_SESSION['tareas'] = $tareas;
+
+header('Location:index.php');
 ?>
